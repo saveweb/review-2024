61291d40e0e6a605a2ccc2fceabcb00b2ace590a
TaranakiNeko <55310629+taranakineko@users.noreply.github.com>
2024-11-29 17:29:15 +0000

    再见啦，2024 /

---
+++ main.py
@@ -0,0 +1,29 @@
+flag = 0
+markdown = """# review-2024
+
+你在期待什么？你在失望什么？
+
+- 2023 年的年终总结见：[saveweb/review-2023](https://github.com/saveweb/review-2023)
+
+保持传统，本项目将长期维护（直到2026年初）。
+
+**想要添加您的年终总结？请发 Issue 或编辑 metadata.md 发 PR**
+
+**（不需要填写博客ID，不需要编辑 README.md）。**
+
+---
+
+"""
+
+raw_lines = open('metadata.md', 'r').read().splitlines()
+
+header = raw_lines[0:2]
+
+lines = set(raw_lines[2:])
+lines.discard('')
+
+with open('README.md', 'w') as f:
+  f.write(markdown+'计数: '+str(len(lines))+' 篇。下表每次 CI 乱序输出。\n\n')
+  f.write('\n'.join(header))
+  f.write('\n')
+  f.write('\n'.join(lines))
