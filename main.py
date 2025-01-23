a772a05cba4de07b32e4f3d3cc6d53b48984deb8
yzqzss <yzqzss@yandex.com>
2025-01-23 23:16:45 +0800

    Nuking Exclusions

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 flag = 0
 markdown = """# review-2024
 
-你在期待什么？你在失望什么？
+WTF happened in 2024?
 
 - 2023 年的年终总结见：[saveweb/review-2023](https://github.com/saveweb/review-2023)
 
 保持传统，本项目将长期维护（直到2026年初）。
 
 **想要添加您的年终总结？请发 Issue 或编辑 metadata.md 发 PR**
+**（不需要填写博客ID，不要编辑 README.md）。**
 
-**如需请求删除收录，请联系 take-down#saveweb.org / info\#saveweb.org / hi\#nekoq.top**
+**删除收录，也可联系 take-down(at)saveweb.org
 
-**（不需要填写博客ID，不需要编辑 README.md）。**
 
 ---
 
