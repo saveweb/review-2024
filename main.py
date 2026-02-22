3d78da085666e534c3b42b4c8b3178b5b7e18d95
GitHub Action <actions@github.com>
2026-02-22 03:39:01 +0100

    init 2025

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ markdown = """# review-2024
 WTF happened in 2024?
 
 - 2023 年的年终总结见：[saveweb/review-2023](https://github.com/saveweb/review-2023)
+- 2024 年：就是这里！
+- 2025 年的年终总结见：[saveweb/review-2025](https://github.com/saveweb/review-2025)
 
 保持传统，本项目将长期维护（直到2026年初）。
 
 **想要添加您的年终总结？请发 Issue 或编辑 metadata.md 发 PR**
 **（不需要填写博客ID，不要编辑 README.md）。**
 
-**删除收录，也可联系 take-down@saveweb.org
+**删除收录，也可联系 take-down[at]saveweb.org
 
 
 ---
